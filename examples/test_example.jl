07b34569d6ade6c48f1c4d466c58c8cf2d721ad7
gkeee <gizemkelender.eee@gmail.com>
2017-10-29 18:47:40 +0300

    Recorrected.

--- examples/test_example.jl
+++ examples/test_example.jl
@@ -1,18 +1,20 @@
 
 include("../src/fractal_interpolation.jl")
 using FractalInterpolation
-using PyPlot
+using Plots, QuadGK
+plotly()
 
 f0(xi) = (y[N] - y[1]) / (x[N] - x[1]) * (xi - x[N]) + y[N]
 x = linspace(0, pi, 1000)
 y = sin.(x)
 N = length(x)
 d = ones(N - 1) * 0.5
-plot(x, y, "o",linewidth=5, label="Data set")
-title("Fractal Interpolation")
-legend(loc="upper right")
-xlabel("x")
-ylabel("y")
+p = plot(x, y)
+
+# title("Fractal Interpolation")
+# legend(loc="upper right")
+# xlabel("x")
+# ylabel("y")
 
 # f_interpolated = interpolate(f0, x, y, d, 1e-3)
 # domain = linspace(x[1], x[end], 1000)
@@ -22,5 +24,6 @@ ylabel("y")
 f_interpolated = interpolate(x, y, d, 1e-6)
 domain = linspace(x[1], x[end], 1000)
 range = f_interpolated.(domain)
-plot(domain, range , label="Interpolate")
-legend(loc="upper right")
+plot!(domain, range , label="Interpolate")
+# legend(loc="upper right")
+display(p)
--- tests/test_hidden_fractal_interpolation.jl
+++ tests/test_hidden_fractal_interpolation.jl
@@ -1,27 +1,45 @@
 include("../src/hidden_fractal_interpolation.jl")
+include("../src/fractal_interpolation.jl")
 
 using HiddenFractalInterpolation
-using Plots
-
-x = [0; 30; 60; 100]
-y = [0.; 50.; 40; 10.]
-H = [0.; 30; 60.; 100.]
-d = [0.3; 0.3; 0.3]
-h = [0.2; 0.2; 0.2]
-l = [-0.1; -0.1; -0.1]
-m = [0.3; 0.; -0.1]
+using FractalInterpolation
+using Plots, QuadGK
+plotly()
+# x = [0; 30; 60; 100]
+x = collect(linspace(0, pi, 10))
+y = sin.(x)
+# H = [0.; 30; 60.; 100.]
+H = x
+n = length(x)
+d = 0.3 * ones(n - 1)
+h = 0.2 * ones(n - 1)
+l = -0.1 * ones(n - 1)
+m = rand(n-1)
+# d = [0.3; 0.3; 0.3]
+# h = [0.2; 0.2; 0.2]
+# l = [-0.1; -0.1; -0.1]
+# m = [0.3; 0.; -0.1]
 
 a, c, e, f, g, k = ω_cof(x, y, H, d, h, l, m)
 
 f0(xi) = [(y[end] - y[1]) / (x[end] - x[1]) * (xi - x[end]) + y[end];
           (H[end] - H[1]) / (x[end] - x[1]) * (xi - x[end]) + H[end]]
-K = 100
+K = 6
 
 fh_interpolated = hidden_interpolate(x, y, H, d, h, l, m, f0, K)
-y_new, z_new = fh_interpolated(x[3])
 domain = linspace(x[1], x[end], 1000)
-range = fh_interpolated.(domain)
-new_range = [range[k][1] for k = 1 : length(range)]
-# pyplot()
-# p = plot(domain, new_range , label="Hidden Interpolate")
-# # legend!(loc="upper right")
+# range = map!(fh_interpolated, domain)
+range_h = fh_interpolated.(domain)
+new_range_h = [range_h[k][1] for k = 1 : length(range_h)]
+
+f_interpolated = interpolate(x, y, d, 1e-3)
+range = f_interpolated.(domain)
+p = plot(domain, range , label="Interpolate")
+plot!(domain, new_range_h , label="Hidden Interpolate",
+    title = "Hidden Variable Fractal Interpolation",
+    xlabel = "x",
+    ylabel = "y" )
+# legend!(loc="upper right")
+scatter!(x,y, label = "Data set")
+display(p)
+@time fh_interpolated.(domain)
